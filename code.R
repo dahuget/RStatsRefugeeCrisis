6a1043a86bbb311f61b6f8957546723d141ba310
dahuget <danaliyahuget@gmail.com>
2018-09-18 20:38:01 -0700

    complete required figures and start report

--- code.R
+++ code.R
@@ -6,7 +6,7 @@ library(maps)
 
 ##Set working directory
 getwd() #First look to see what is the current directory 
-setwd("/Users/chris/Desktop") #Then set the directory to where you want to retrieve and print data
+#setwd("/Users/chris/Desktop") #Then set the directory to where you want to retrieve and print data
 
 ##Read in data
 df <-  data.frame(read.csv("events.csv"), header=TRUE)  #read csv file
@@ -20,6 +20,11 @@ head(df)
 ##Mean
 meanPop <- mean(df$dead_and_missing) #population mean
 mean2015 <- mean(subset(df, Year == 2015)$dead_and_missing) #2015 mean
+# Create the vector. x <- c(12,7,3,4.2,18,2,54,-21,8,-5)
+x <- c(df$dead_and_missing)
+# Find the median. median.result <- median(x)
+medianPop <- median(df$dead_and_missing)
+median2015 <- as.numeric(median(subset(df, Year == 2015)$dead_and_missing))
 
 #Standard Deviation
 sdPop <- sd(df$dead_and_missing) #population standard deviation
@@ -34,11 +39,12 @@ modevalues2015<- values2015$dead_and_missing #extract the variable for dead and 
 mode2015 <- as.numeric(names(sort(-table(modevalues2015)))[1]) #sort the dataset and read the first row (most frequent)
 
 #Creating table
-samples = c("Population", "2015") #Create an object for the labels
-means = c(meanPop, mean2015) #Create an object for the means
-sd = (c(sdPop, sd2015))
-mode = (c(modePop, mode2015))
-data.for.table = data.frame(samples, means, sd, mode)
+Samples = c("Population", "2015") #Create an object for the labels
+Mean = c(meanPop, mean2015) #Create an object for the means
+Median = (c(medianPop, median2015))
+Mode = (c(modePop, mode2015))
+`Std Dev` = (c(sdPop, sd2015))
+data.for.table = data.frame(Samples, Mean, Median, Mode, `Std Dev`)
 
 #Printing a table (you can use the same setup for printing other types of objects
 png("Output_Table.png") #Create an object to print the table to
@@ -46,13 +52,24 @@ grid.table(data.for.table, row.names(NULL)) #Create table
 dev.off() #Print table
 
 #Create and print a histogram
+#hist(df$dead_and_missing, breaks = 20, main = "Dana Huget's Histogram", xlab = "Number of Dead and Missing")
 
-hist(df$dead_and_missing, breaks = 20, main = "Dana Huget's Histogram", xlab = "Number of Dead and Missing")
+#You will see that this histogram is highly positively skewed, as the vast majority of records have a value of 1. 
+#In order to get a better sense of how the data is distributed, we can graph the log value of each record. 
+#We do this in the code below that prints a histogram with log values.
 
-#You will see that this histogram is highly positively skewed, as the vast majority of records have a value of 1. In order to get a better sense of how the data is distributed, we can graph the log value of each record. We do this in the code below that prints a histogram with log values.
+pdf("Output_Histogram_.pdf")
+#qplot(df$dead_and_missing, geom="histogram", main = "Dana Huget's Histogram",
+     # xlab = "Number of Dead and Missing", ylab = "Frequency") + scale_x_continuous(trans="log10")
+#qplot(df$dead_and_missing, geom="histogram", main = "Dana Huget's Histogram",
+     # xlab = "Number of Dead and Missing", ylab = "Frequency") + scale_y_log10()
+qplot(df$dead_and_missing, geom="histogram", main = "Dana Huget's Histogram",
+      xlab = "Number of Dead and Missing", ylab = "Frequency", log = "y") #+ geom_bar(fill="#FF9999", colour="black")
+dev.off() 
 
 png("Output_Histogram.png") #Create an object to print the table to
-hist(log(df$dead_and_missing), breaks = 20, main = "Dana Huget's Histogram", xlab = "Number of Dead and Missing")
+hist(df$dead_and_missing, breaks = 20, main = "Dana Huget's Histogram", xlab = "Number of Dead and Missing")
+#hist(log(df$dead_and_missing), breaks = 20, main = "Dana Huget's Histogram", xlab = "Number of Dead and Missing")
 #hist(log(df$dead_and_missing), breaks = 20, main = "Dana Huget's Histogram", xlab = "Number of Dead and Missing", ylim=c(0,200))
 dev.off() #Print histogram
 
@@ -85,6 +102,33 @@ mp <- mp+ geom_point(aes(x=df$longitude, y=df$latitude) ,color="blue", size = lo
 mp
 
 #Slightly better looking. Here is another type of map that we can create with a more aesthetically pleasing basemap.
-world.map <- get_map(location = c(lon = -40.0, lat = 20.0), zoom = 4)
 #world.map <- get_map(location = c(lon = -40.0, lat = 20.0), zoom = 4)
-ggmap(world.map) + geom_point(data = df, aes(x = df$longitude, y = df$latitude, size = df$dead_and_missing))
+world.map <- get_map(location = c(lon = -40.0, lat = 20.0), zoom = 2)
+
+library(Hmisc)
+label(df$dead_and_missing)
+describe(df$dead_and_missing)
+#Remove labels:
+Dead_and_Missing <- clear_labels(df$dead_and_missing)
+`Number of Dead and Missing` = df$dead_and_missing
+world.map <- get_map(location = c(lon = mean(range(df$longitude)), lat = mean(range(df$latitude))), zoom = 3)
+ggmap(world.map) + ggtitle("Dana Huget's Map") + 
+  theme(plot.title = element_text(size = 12, face = "bold", hjust = 0.5)) +
+  geom_point(data = df, aes(x = df$longitude, y = df$latitude, size = `Number of Dead and Missing`, colour = `Number of Dead and Missing`)) +
+  scale_colour_gradient( high = "black", low = "lightpink")
+
+ggmap(world.map) + ggtitle("Dana Huget's Map") +
+  geom_point(data = df, aes(x = df$longitude, y = df$latitude, colour = `Number of Dead and Missing`)) +
+  scale_colour_gradient( high = "black", low = "lightpink")
+
+meanLat <- mean(df$latitude)
+meanLon <- mean(df$longitude)
+Longitude <- df$longitude
+ggmap(world.map) + ggtitle("Refugee Crisis Map") + 
+  theme(plot.title = element_text(size = 16, face = "bold", hjust = 0.5)) +
+  geom_point(data = df, aes(x = df$longitude, y = df$latitude, size = `Number of Dead and Missing`, colour = `Number of Dead and Missing`)) +
+  scale_colour_gradient( high = "black", low = "lightpink") +
+  geom_point(aes(x = meanLon, y = meanLat), data = df, shape=23, fill="red", color="darkred", size=2) +
+  #label = "italic(R) ^ 2 == 0.75", parse = TRUE
+  annotate("text", label = "Mean Centre", fontface=2, x = meanLon+1, y = meanLat, hjust = 0, color = "white", size=3)
+
